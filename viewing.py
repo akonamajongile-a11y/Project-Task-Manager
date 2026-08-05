a0e557f233d92de709368b139c6865d878883647
SumiK2001 <sumaya2001khan@gmail.com>
2026-08-05 14:21:58 +0200

    edit

--- viewing.py
+++ viewing.py
@@ -1,9 +1,11 @@
 def view_task():
     import csv
-    with open("tasks.csv" , "r" , "newlines") as file:
+    with open("tasks.csv" , "r" , newline="") as file:
         reader = csv.reader(file)
 
         for everything in reader:
             print("Task ID: " + "Task Name" + "Category" + "Description: " + "Priority: " + "Due Date: " + "Status: ")
             print(everything[0] + everything[1] + everything[2] + everything[3] + everything[4] + everything[5] + everything[6])
-            
+
+
+
