ef8880f20bdeeb3bb40c2bb3b25e75297ecb31a4
Phaks323@gmail.com <phaks323@gmail.com@gmail.com>
2026-08-05 16:12:19 +0200

    fdsc

--- search.py
+++ search.py
@@ -1,9 +1,13 @@
 def search_engine():
     #print("Search by ")
     import csv
-    with open("task.csv", "r", newline="") as file: 
+    with open("tasks.csv", "r", newline="") as file: 
                     reader =csv.reader(file)
 
                     for e in reader:
-                        print("Search by:")
+                        if e[1] == " sewing":
+                            print(e)
+                            break
+                        else:
+                            print("Checking...")
 search_engine()
