41f664786c4bcc29707a12f540f58ab212021b3c
melvin-maker-ally <Melvinradile5@gmail.com>
2026-08-05 09:28:37 +0200

    I have accepeted the inivation and added some changes to the add_feature

---
+++ add_feature.py
@@ -0,0 +1,10 @@
+#List to store tasks
+List = []
+
+title = input("Please input your tasks name:")
+description = input("Please enter the description of the task: ")
+due_date = input("The due date of the task is: ")
+
+
+         
+
