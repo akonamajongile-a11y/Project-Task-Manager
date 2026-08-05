f83a09d1841b8bd932d813ff7a1d6b8a6a123c92
SumiK2001 <sumaya2001khan@gmail.com>
2026-08-05 14:57:56 +0200

    Fixed function

--- viewing.py
+++ viewing.py
@@ -3,9 +3,8 @@ def view_task():
     with open("tasks.csv" , "r" , newline="") as file:
         reader = csv.reader(file)
 
+        print("Task ID: " + "Task Name: " + "Category: " + "Description: " + "Priority: " + "Due Date: " + "Status: ")
         for everything in reader:
-            print("Task ID: " + "Task Name" + "Category" + "Description: " + "Priority: " + "Due Date: " + "Status: ")
-            print(everything[0] + everything[1] + everything[2] + everything[3] + everything[4] + everything[5] + everything[6])
-
+            print(everything)
 
 
