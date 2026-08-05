4fae2914e8bab30e72d3997b73175d7f74848cfc
melvin-maker-ally <Melvinradile5@gmail.com>
2026-08-05 16:21:14 +0200

    On the priority feature, the user can only insert between the three options presented. If the user inserts a option other than what is indicated then an error message will display.

--- add_feature.py
+++ add_feature.py
@@ -14,7 +14,6 @@ def add_tasks():
     valid = False
     while not valid:
         task_priority = input("Please select the priority level (Low/Medium/High): ").capitalize()
-
         if task_priority == "Low": 
                     valid = True
                     print("You have selected: Low" )
@@ -22,17 +21,21 @@ def add_tasks():
                 valid = True
                 print("You have selected: Medium")
         elif task_priority == "High":
-                valid =True
+                valid = True
                 print("You have selected: High")
         else:
             print("Please insert correct priority option!")
+
     task_due_date = input("The due date of the task is (DD/MM/YYY): ")
+    import datetime 
+    task_due_date = datetime.datetime.now()
+    print(task_due_date.year)
+    print(task_due_date.strftime("%A"))
+
     task_status = input("Please select a status level (Not Started/ Started/ Done): ").capitalize()
     import csv 
 
     with open("tasks.csv", "a", newline="") as file: 
             writer =csv.writer(file)
             writer.writerows([[task_ID, task_name, task_description, task_due_date, task_priority, task_category,task_status]])
-
-
-
+add_tasks()
