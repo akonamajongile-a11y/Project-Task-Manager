1a4f498eb7a5a598aa9e20c0ee2859c7d2713cab
melvin-maker-ally <Melvinradile5@gmail.com>
2026-08-05 16:30:29 +0200

    On the priority feature, the user can only insert between the three options presented. If the user inserts a option other than what is indicated then an error message will display.

--- add_feature.py
+++ add_feature.py
@@ -9,33 +9,33 @@ def add_tasks():
 
     task_ID = count + 1
     task_name = input("Please input your tasks name:").capitalize()
-    task_category = input("Please select the category (Work/Personal)").capitalize()
-    task_description = input("Please enter the description of the task: ").capitalize()
+    category = input("Please select the category (Work/Personal)").capitalize()
+    description = input("Please enter the description of the task: ").capitalize()
     valid = False
     while not valid:
-        task_priority = input("Please select the priority level (Low/Medium/High): ").capitalize()
-        if task_priority == "Low": 
+        priority = input("Please select the priority level (Low/Medium/High): ").capitalize()
+        if priority == "Low": 
                     valid = True
                     print("You have selected: Low" )
-        elif task_priority == "Medium":
+        elif priority == "Medium":
                 valid = True
                 print("You have selected: Medium")
-        elif task_priority == "High":
+        elif priority == "High":
                 valid = True
                 print("You have selected: High")
         else:
             print("Please insert correct priority option!")
 
-    task_due_date = input("The due date of the task is (DD/MM/YYY): ")
+    due_date = input("The due date of the task is (DD/MM/YYY): ")
     import datetime 
-    task_due_date = datetime.datetime.now()
-    print(task_due_date.year)
-    print(task_due_date.strftime("%A"))
+    due_date = datetime.datetime.now()
+    print(due_date.year)
+    print(due_date.strftime("%A"))
 
-    task_status = input("Please select a status level (Not Started/ Started/ Done): ").capitalize()
+    status = input("Please select a status level (Not Started/ Started/ Done): ").capitalize()
     import csv 
 
     with open("tasks.csv", "a", newline="") as file: 
             writer =csv.writer(file)
-            writer.writerows([[task_ID, task_name, task_description, task_due_date, task_priority, task_category,task_status]])
+            writer.writerows([[task_ID, task_name, description, due_date, priority, category, status]])
 add_tasks()
