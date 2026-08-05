607bb57a3703d70ec70c385d8ab276d8864119a4
melvin-maker-ally <Melvinradile5@gmail.com>
2026-08-05 10:27:38 +0200

    I have added some changes to the file

--- add_feature.py
+++ add_feature.py
@@ -1,11 +1,13 @@
-#List to store tasks
-Tasks = []
+
+Tasks = [] # The user inputs different tasks. 
 
 #Create the variable 
 Task_name = input("Please input your tasks name:")
 Taks_description = input("Please enter the description of the task: ")
-Task_due_date = input("The due date of the task is: ")
-Task_Status = 
-Tasks_Category = 
+Task_due_date = input("The due date of the task is (DD/MM/YYY): ")
 
-         
+
+# Adding the Function 
+
+
+
