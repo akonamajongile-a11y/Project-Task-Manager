1fe7657e7700b4a30fbb58634e5a9dd6945f982b
SumiK2001 <sumaya2001khan@gmail.com>
2026-08-06 11:20:28 +0200

    Update format

--- viewing.py
+++ viewing.py
@@ -1,22 +1,32 @@
 
 def view_task():
     import csv
-    from colorama import Fore, Style
     with open("tasks.csv" , "r" , newline="") as file:
         reader = csv.reader(file)
-        task_categories = "Task ID:       " + "Task Name:      " + "Category:      " + "Description:     " + "Priority:     " + "Due Date:      " + "Status:      "
+        task_categories = (
+            f"{'Task ID':<10}"
+            f"{'Task Name':<20}"
+            f"{'Category':<12}"
+            f"{'Description':<35}"
+            f"{'Priority':<10}"
+            f"{'Due Date':<15}"
+            f"{'Status':<10}")
         print(task_categories)
         print("-" * len(task_categories))   # Seperator line
 
         for everything in reader:
-            print(everything[0]   + everything[1]   + everything[2]   + everything[3]   + everything[4]   + everything[5]) 
+            print(
+                f"{everything[0]:<10}"
+                f"{everything[1]:<20}"
+                f"{everything[2]:<12}"
+                f"{everything[3]:<35}"
+                f"{everything[4]:<10}"
+                f"{everything[5]:<15}"
+                f"{everything[6]:<10}")
+            
     print("Keep going, you're doing great!")
 
-    priority = everything[4]
-    if priority.lower() == "Medium":
-                priority_text = f"{Fore.RED}{priority}{Style.RESET_ALL}"
-    else:
-                priority_text = f"{priority}"
+    
        
        
 view_task()
