1c936272b072822a77e3f059e8ec9f00103a1ca2
SumiK2001 <sumaya2001khan@gmail.com>
2026-08-06 08:53:24 +0200

    changes

--- viewing.py
+++ viewing.py
@@ -6,6 +6,7 @@ def view_task():
 
         print("Task ID:       " + "Task Name:      " + "Category:      " + "Description:     " + "Priority:     " + "Due Date:      " + "Status:      ")
         for everything in reader:
-            print(everything[0]   + everything[1]   + everything[2]   + everything[3]   + everything[4]   + everything[5] + everything[6]) 
-
-
+            print(everything[0]   + everything[1]   + everything[2]   + everything[3]   + everything[4]   + everything[5]) 
+    print("Keep going, you're doing great!")
+        
+view_task()
