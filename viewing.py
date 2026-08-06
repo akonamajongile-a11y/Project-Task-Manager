f76343327bb5ef767f4f729b290ede7543f84074
akonamajongile-a11y <akonamajongile@gmail.com>
2026-08-06 14:51:56 +0200

    gdr

--- viewing.py
+++ viewing.py
@@ -16,9 +16,6 @@ def view_task():
         print("-" * len(task_categories))   # Seperator line
 
         for everything in reader:
-<<<<<<< HEAD
-            print(everything)
-=======
             print(
                 f"{everything[0]:<10}"
                 f"{everything[1]:<20}"
@@ -40,4 +37,3 @@ def view_task():
        
        
 view_task()
->>>>>>> 5e39284666a11b9b53a776afa893aeb5ed94c631
