e7991fa47aa1ea6e437a7cb54b8ef7a97e3f3121
LuckyTwala0000 <snazolucky@gmail.com>
2026-08-06 14:28:41 +0200

    New latest

--- update.py
+++ update.py
@@ -8,15 +8,43 @@ def update():
 
         for line in reader:
             if line[0] == task_ID:  
+                option = input("""
+                        1. Task Name
+                        2. Category: Work/Personal
+                        3. Description
+                        4. Priority: High/Medium/Low
+                        5. Due Date
+                        6. Status: Not Started/Started/Done
+                        """)
 
-                task_name = input("Enter the new task name: ")
-
-                line[1] = task_name
+                
+                if option == "1":
+                    task_name = input("Enter the new task name: ")
+                    line[1] = task_name
+                elif option == "2":
+                    category = input("Enter the new category: ")
+                    line[2] = category
+                elif option == "3":
+                    description = input("Enter the new description: ")
+                    line[3] = description 
+                elif option == "4":
+                    priority = input("Enter the new priority: ")
+                    line[4] = priority 
+                elif option == "5":
+                    due_date = input("Enter the new due_date: ")
+                    line[5] = due_date 
+                elif option == "6":
+                    status = input("Enter the new status : ")
+                    line[6] = status  
+                else:
+                    print("Please enter a valid option! ") 
+              
                
                 with open("tasksnew.csv", "a", newline="") as file:
-                                writer = csv.writer(file)
-                                writer.writerow(line)
-                print("Task updated successfully.")
+                   writer = csv.writer(file)
+                   writer.writerow(line)
+                   print("Task updated successfully.")
+
             else:
 
                 with open("tasksnew.csv", "a", newline="") as file:
