59ad8124dd8984250a32a5f3db50f0220cec7a9c
melvin-maker-ally <Melvinradile5@gmail.com>
2026-08-07 21:55:59 +0200

    I have added a line of code whereby if the task is overdue then a message will return

--- notifications.py
+++ notifications.py
@@ -1,11 +1,28 @@
-from datetime import datetime
-
 def notifications(): 
-    today = datetime.today().date()
-    print(" ======== TASK NOTIFICATIONS ========")
-    notifications = False
-    for tasks in tasks: 
-        due_date = datetime.strptime(tasks["due date"], "%d/%m/").date()
+        import csv 
+        count = 0
+        with open("tasks.csv", "r", newline="") as file: 
+                reader =csv.reader(file)
+                for e in reader: 
+                       count = count + 1
+                       print(" ======== TASK NOTIFICATIONS ========")
+        from datetime import datetime 
+        today = "07/08/2026"
+        due_date = False
+        while not due_date: 
+                due_date = input("The due date of the task is (DD/MM/YYY): ")
+                if due_date > today:
+                        print("You are overdue with your tasks")
+                else:
+                        if due_date < str(14):
+                                print("Please note that your task is close to the due date")
+
+
+
+
+
+
+
 
 
 
