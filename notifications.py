3bc88620a24d83f422516cd080cc072c30c66d40
akonamajongile-a11y <akonamajongile@gmail.com>
2026-08-11 15:48:10 +0200

    Changes

--- notifications.py
+++ notifications.py
@@ -11,11 +11,11 @@ def notifications():
             for e in reader:
                 task_id = e[0]
                 today = datetime.now().date()
-                due_date = datetime.strptime(e[5], "%Y-%m-%d").date()
+                due_date = datetime.strptime(e[5], "%d/%m/%Y").date()
                 days_left = (due_date - today).days
                 if days_left < 0:
-                    print(task_id, "You are overdue with your tasks")
+                    print("Task ID", task_id, "is overdue")
                 elif days_left <= 2:
-                    print(task_id, "Please note that your task is close to the due date")
+                    print("Task ID", task_id, "is close to the due date")
 
        
