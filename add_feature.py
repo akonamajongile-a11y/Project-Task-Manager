33f62ac02174d0e364f6ca55ba29ff87b6dce7c0
melvin-maker-ally <Melvinradile5@gmail.com>
2026-08-11 13:16:51 +0200

    Changes were made.

--- add_feature.py
+++ add_feature.py
@@ -86,4 +86,4 @@ def add_tasks():
     with open("tasks.csv", "a", newline="") as file: 
             writer =csv.writer(file)    
             writer.writerows([[task_ID, task_name, category, description, priority, due_date, status]])
-
+add_tasks()
