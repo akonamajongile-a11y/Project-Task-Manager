2374ff4cfa1a4693067b2a153bbe085091ad08c4
melvin-maker-ally <Melvinradile5@gmail.com>
2026-08-11 11:33:28 +0200

    Resoved: Notification (11-08-2026 11:33 SAST)

--- notifications.py
+++ notifications.py
@@ -1,41 +1,21 @@
+import csv 
+from datetime import datetime 
 def notifications(): 
-        from datetime import datetime 
-        print(" ======== TASK NOTIFICATIONS ========")
-        today = datetime.now().date()
-        import csv 
-        count = 0
+       
+        
+        today = datetime.today().date()
+        
         with open("tasks.csv", "r", newline="") as file: 
-                reader =csv.reader(file)
-                for e in reader: 
-                        count = count + 1
+            reader = csv.reader(file)
 
-        due_date = datetime.strptime("due_date" , "%d/%m/%Y").date()
-        days_remaining = (due_date - today).days
-        if days_remaining < 0:
-                print("You are overdue with your tasks")
-        elif days_remaining <= 7:
-                print("Please note that your task is close to the due date")
-                
-        valid_date = False
-        while not valid_date:
-                if due_date == False:
-                        from datetime import datetime 
-                try: 
-                        if len(due_date) != 10:
-                                raise ValueError("Incorrect format length")
-                        date_obj = datetime.strptime(due_date, "%d/%m/%Y")
-                        print("Valid date: ", date_obj)
-                except ValueError as e: 
-                                print("Error: ", e)
-                if due_date == False:
-                        valid_date = True
-                        print("You have entered a valid date")
-                if due_date > today:
-                        valid_date = False
-                        print("You are overdue with your tasks")
-                else:
-                        valid_date = True
-                        print("Please note that your task is close to the due date")   
-
-
+            for e in reader:
+                task_id = e[0]
+                today = datetime.now().date()
+                due_date = datetime.strptime(e[5], "%Y-%m-%d").date()
+                days_left = (due_date - today).days
+                if days_left < 0:
+                    print(task_id, "You are overdue with your tasks")
+                elif days_left <= 2:
+                    print(task_id, "Please note that your task is close to the due date")
 notifications()
+       
