dec69ee17b88837022b60afde15bc8ceb03d21ed
SumiK2001 <sumaya2001khan@gmail.com>
2026-08-12 14:54:07 +0200

    Final changes

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,5 @@
+import csv
+
 def task_dashboard():
     dash_categories = ""
     total_tasks = 0
@@ -12,7 +14,6 @@ def task_dashboard():
 
     from datetime import datetime
     today = datetime.today().date()  # current date
-    import csv
     with open("tasks.csv" , "r" , newline="") as file:
         reader = csv.reader(file)
 
@@ -21,21 +22,20 @@ def task_dashboard():
             total_tasks += 1
             status = everything[6].lower() 
             due_date = datetime.strptime(everything[5], "%d/%m/%Y").date()
-        
+
             # Count Completed Tasks: "Done"
             if everything[6] == "Done":
                 completed_tasks += 1
 
                 #Count Pending Tasks: "Pending"
-            elif everything[6] == "Pending" and due_date > today:
+            elif everything[6] == "Pending":
                 pending_tasks +=1
               
 
             # Count overdue tasks: Pending & due date is before today
-            elif everything[6] == "Pending" and due_date < today:
+            if  due_date < today:
                 overdue_tasks +=1
 
-    import csv
     with open("tasks.csv" , "r" , newline="") as file:
         reader = csv.reader(file)
 
@@ -51,38 +51,38 @@ def task_dashboard():
             elif everything[2] == "Personal":
                 personal_tasks +=1  
 
-                
-
-    print("=====================================")
-    print(" " * 11 + "Task Dashboard")
-    print("=====================================")
-    print(f"Total Tasks: {total_tasks}")
-    print(f"Completed Tasks: {completed_tasks}")
-    print(f"Incomplete Tasks: {pending_tasks}")
-    print(f"Overdue Tasks: {overdue_tasks}")
-    print("=====================================")
-    print(f"Work Tasks: {work_tasks}")
-    print(f"Personal Tasks: {personal_tasks}")
-    print("=====================================")
-
-# Calculate percentages
+            # Calculate percentages
     if total_tasks > 0:
         completed_percentage = (completed_tasks / total_tasks) * 100
         overdue_percentage = (overdue_tasks / total_tasks) * 100
         pending_percentage = (pending_tasks / total_tasks) * 100
 
         work_percentage = (work_tasks / total_tasks) * 100
-        personal_percentage = (personal_tasks / total_tasks) * 100
+        personal_percentage = (personal_tasks / total_tasks) * 100    
 
+
+            # Print no. of tasks
+    print("=====================================")
+    print(" " * 11 + "Task Dashboard")
+    print("====================================")
+    print(f"Total Tasks: {total_tasks}")
+    print(f"Completed Tasks: {completed_tasks}")
+    print(f"Incomplete Tasks: {pending_tasks}")
+    print("====================================")
+    print(f"Overdue Tasks: {overdue_tasks} ({overdue_percentage:.2f}%)")
+    print("====================================")
+    print(f"Work Tasks: {work_tasks}")
+    print(f"Personal Tasks: {personal_tasks}")
+    print("====================================")
+
+           # Print Distribution of tasks
     print(f"Distribution Of Tasks Per Status:")
     print(f"% of Completed Tasks: ({completed_percentage:.2f}%)")
     print(f"% of Incomplete Tasks: ({pending_percentage:.2f}%)")
-    print(f"% of Overdue Tasks: ({overdue_percentage:.2f}%)")
     print("====================================")
     print(f"Distribution Of Tasks Per Category:")
     print(f"% of Work Tasks: ({work_percentage:.2f}%)")
     print(f"% of Personal Tasks: ({personal_percentage:.2f}%)")
     print("====================================")
     print("Keep going, you’re doing great :)")
-
-task_dashboard()
+    print("====================================")
